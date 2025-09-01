a8adec6ba318ffd459bf1dc6f910fddb5bad3e28
Abdelrhman Osama Mostafa Shawky <abdalrhman.osama.mostafa@gmail.com>
2025-09-02 02:54:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const db = require("./init-db"); // اتصال قاعدة البيانات
+const db = require("./init-db"); 
 
 const app = express();
 app.use(bodyParser.json());
